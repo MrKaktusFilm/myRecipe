15e7c99993f7fee0e476e6a1d2b8425dd351f7f2
MrKaktusFilm <marko.wy@gmx.de>
2025-05-31 10:28:29 +0200

    add comment section

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({  
     server: {
         port: 3000,
+        host: true,
     },
     plugins: [    tailwindcss(),  ],
 })
